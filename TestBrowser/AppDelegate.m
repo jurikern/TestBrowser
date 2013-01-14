a9305ee872491a652c9c316dc484f1874c4992c6
Juri Semjonov <juri.semjonov@gmail.com>
2013-01-14 10:04:37 +0200

    Initial Commit

---
+++ TestBrowser/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  TestBrowser
+//
+//  Created by Juri Semjonov on 1/14/13.
+//  Copyright (c) 2013 Juri Semjonov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ TestBrowser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TestBrowser
+//
+//  Created by Juri Semjonov on 1/14/13.
+//  Copyright (c) 2013 Juri Semjonov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
